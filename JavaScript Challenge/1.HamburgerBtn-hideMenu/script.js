59db3173f1cbcbdf19c697a4bacf83b2dbe60a65
jjyoon-dev <jjyoon.dev@gmail.com>
2021-08-03 23:37:15 +0900

    not need

--- JavaScript Challenge/1.HamburgerBtn-hideMenu/script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById('btn');
-const nav = document.getElementById('nav');
-
-btn.addEventListener('click', function(){
-	nav.classList.toggle('active');	btn.classList.toggle('active');
-});
--- JavaScript Challenge/7.Dark-mode-toggle/script.js
+++
@@ -1,5 +0,0 @@
-const toggle = document.getElementById('toggle');
-
-toggle.addEventListener('change', function(e) {
-	document.body.classList.toggle('dark', e.target.checked);
-})
