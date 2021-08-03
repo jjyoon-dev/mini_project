ca4b01f88f01db7684e6491a0d4005e847d3f720
jjyoon-dev <jjyoon.dev@gmail.com>
2021-08-04 02:56:25 +0900

    wrong file del

--- Nav_bar/Tutorial2/script/nav_bar.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-	let menuToggle = document.body.querySelector(".menu-toggle");
-	let nav = document.body.querySelector(".nav");
-	
-	menuToggle.addEventListener("click", function() {
-		menuToggle.classList.toggle("active");	
-		nav.classList.toggle("active");
-		});
-	});	
